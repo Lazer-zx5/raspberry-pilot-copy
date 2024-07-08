a50d56a5bf533fcb03139e72a6e96f1b0062f30d
Lazer-zx5 <balagyozyanlevon@gmail.com>
2024-07-08 02:11:51 -0500

    Changed the file structure, updated readme, added a prioritized todo list

--- launch_tesla.sh
+++
@@ -1,8 +0,0 @@
-!/bin/bash
-cd ~/raspilot
-export PYTHONPATH="$PWD"
-pkill -f GernbyMode
-
-# Try both versions... the one that matches the hardware will win
-bin/python3 ~/raspilot/controls_PICAN.py
-bin/python3 ~/raspilot/controls_Panda.py
